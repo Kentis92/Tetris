2cddcf67d2ea2061f1d74b82e200fac155a4e329
Kentis92 <kentvidar.26pg@gmail.com>
2026-08-31 10:51:29 +0200

    la til kontroller på brikker.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
+using Tetris.Models;
 
 namespace Tetris;
 
@@ -9,10 +12,30 @@ public partial class MainWindow : Window
     private const int GridWidth = 10;
     private const int GridHeight = 20;
 
+    private readonly Border[,] cells = new Border[GridWidth, GridHeight];
+    private readonly DispatcherTimer gameTimer;
+    private TetrisPiece currentPiece;
+
     public MainWindow()
     {
         InitializeComponent();
+
         CreateGameBoard();
+
+        currentPiece = new TetrisPiece();
+
+        DrawPiece();
+
+        gameTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(500)
+        };
+
+        gameTimer.Tick += GameTimer_Tick;
+        gameTimer.Start();
+
+        KeyDown += MainWindow_KeyDown;
+        Focus();
     }
 
     private void CreateGameBoard()
@@ -28,7 +51,77 @@ public partial class MainWindow : Window
                     BorderThickness = new Thickness(1)
                 };
 
+                cells[x, y] = cell;
                 GameBoard.Children.Add(cell);
+            }
+        }
+    }
+
+    private void GameTimer_Tick(object? sender, EventArgs e)
+    {
+        currentPiece.Y++;
+
+        DrawBoard();
+    }
+
+    private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Left:
+                currentPiece.X--;
+                break;
+
+            case Key.Right:
+                currentPiece.X++;
+                break;
+
+            case Key.Down:
+                currentPiece.Y++;
+                break;
+
+            case Key.Up:
+                currentPiece.Rotate();
+                break;
+        }
+
+        DrawBoard();
+    }
+
+    private void DrawBoard()
+    {
+        ClearBoard();
+        DrawPiece();
+    }
+
+    private void DrawPiece()
+    {
+        for (int y = 0; y < currentPiece.Shape.GetLength(0); y++)
+        {
+            for (int x = 0; x < currentPiece.Shape.GetLength(1); x++)
+            {
+                if (currentPiece.Shape[y, x] == 1)
+                {
+                    int boardX = currentPiece.X + x;
+                    int boardY = currentPiece.Y + y;
+
+                    if (boardX >= 0 && boardX < GridWidth &&
+                        boardY >= 0 && boardY < GridHeight)
+                    {
+                        cells[boardX, boardY].Background = Brushes.Cyan;
+                    }
+                }
+            }
+        }
+    }
+
+    private void ClearBoard()
+    {
+        for (int y = 0; y < GridHeight; y++)
+        {
+            for (int x = 0; x < GridWidth; x++)
+            {
+                cells[x, y].Background = Brushes.Black;
             }
         }
     }
---
+++ Models/TetrisPiece.cs
@@ -0,0 +1,38 @@
+namespace Tetris.Models;
+
+public class TetrisPiece
+{
+    public int[,] Shape { get; private set; }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public TetrisPiece()
+    {
+        Shape = new int[,]
+        {
+            { 1, 1, 1, 1 }
+        };
+
+        X = 3;
+        Y = 0;
+    }
+
+    public void Rotate()
+    {
+        int rows = Shape.GetLength(0);
+        int columns = Shape.GetLength(1);
+
+        int[,] rotated = new int[columns, rows];
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                rotated[x, rows - 1 - y] = Shape[y, x];
+            }
+        }
+
+        Shape = rotated;
+    }
+}
